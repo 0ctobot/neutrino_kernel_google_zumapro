0a5532df5452783a7bc017ab58c4631df634077d
Ken Lin <lyenting@google.com>
2024-06-14 02:10:34 +0000

    cm4a: correct the mode to wait_for_vsync_done while exiting AOD
    Bug: 346215549
    Test: enter/exit AOD mode
    Change-Id: I08fcc830c02b1d4f028c1c79f3ba9dab31b11990
    Signed-off-by: Ken Lin <lyenting@google.com>

--- panel-gs-cm4.c
+++ panel-gs-cm4.c
@@ -1703,7 +1703,7 @@ static void cm4_set_lp_mode(struct gs_panel *ctx, const struct gs_panel_mode *pm
 
 	/* TODO(b/336580972): Support minRR and FI setting during AOD */
 
-	cm4_wait_for_vsync_done(ctx, pmode);
+	cm4_wait_for_vsync_done(ctx, ctx->current_mode);
 	GS_DCS_BUF_ADD_CMDLIST(dev, unlock_cmd_f0);
 	GS_DCS_BUF_ADD_CMDLIST(dev, aod_on);
 	/* Fixed TE: sync on */
@@ -1742,16 +1742,16 @@ static void cm4_set_nolp_mode(struct gs_panel *ctx, const struct gs_panel_mode *
 
 	DPU_ATRACE_BEGIN(__func__);
 
-	cm4_wait_for_vsync_done(ctx, pmode);
+	cm4_wait_for_vsync_done(ctx, ctx->current_mode);
 	/* manual mode 30Hz */
 	cm4_enforce_manual_and_peak(ctx);
 
-	cm4_wait_for_vsync_done(ctx, pmode);
+	cm4_wait_for_vsync_done(ctx, ctx->current_mode);
 	GS_DCS_BUF_ADD_CMDLIST(dev, unlock_cmd_f0);
 	GS_DCS_BUF_ADD_CMDLIST(dev, aod_off);
 	GS_DCS_BUF_ADD_CMDLIST_AND_FLUSH(dev, lock_cmd_f0);
 
-	cm4_wait_for_vsync_done(ctx, pmode);
+	cm4_wait_for_vsync_done(ctx, ctx->current_mode);
 #ifndef PANEL_FACTORY_BUILD
 	cm4_update_refresh_ctrl_feat(ctx, pmode);
 #endif
